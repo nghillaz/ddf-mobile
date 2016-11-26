835a259da2cdaf5d5507617421d2cd1bdbba9517
DrCopernicus <nicholas.copernicus.lee@gmail.com>
2016-11-26 02:36:44 -0700

    Updated for React Native 0.38 and added a login page (nonfunctional, only UI)

--- index.android.js
+++ index.android.js
@@ -6,109 +6,126 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  ListView
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ListView,
+    TextInput,
+    Button,
+    Alert
 } from 'react-native';
-import Button from 'react-native-button';
 
-class DisplayItemsBox extends Component {
-    constructor() {
-        super();
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = { dataSource: ds.cloneWithRows(['row 1', 'row 2']), };
-    }
+const onButtonPress = () => { Alert.alert('Button has been pressed!'); };
 
+class LoginScreen extends Component {
     render() {
         return (
-            <ListView
-                dataSource={this.state.dataSource}
-                renderRow={(rowData) => <Text>{rowData}</Text>}
-            />
-        );
-    }
-}
-
-class ToDoItem extends Component {
-    render() {
-        return (
-            <View style={styles.littlebox}>
-                <Text>
-                    This is a seperate component!
-                </Text>
+            <View style={styles.container}>
+                <Text style={styles.titleBar}>Login</Text>
+                <View style={styles.loginContent}>
+                    <Text style={styles.fieldLabel}>Username</Text>
+                    <TextInput style={styles.textBox}/>
+                    <Text style={styles.fieldLabel}>Password</Text>
+                    <TextInput style={styles.textBox}/>
+                    <Button onPress={onButtonPress} title="Login" accessibilityLabel="Login"/>
+                </View>
             </View>
         );
     }
 }
 
-class AddItemBar extends Component {
-  render() {
-    return (
-      <View style={styles.additembar}>
-        <Button>
-            +
-        </Button>
-      </View>
-    );
-  }
-  
-  _handlePress() {
-    
-  }
+class EditQueryScreen extends Component {
+    render() {
+        return (
+            <Text>
+                This is a seperate component!!
+            </Text>
+        );
+    }
+}
+
+class TabularResultsScreenOverview extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+class MapResultsScreenOverview extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
+
+class TabularResultsScreenIndividual extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
+}
+
+class MapResultsScreenIndividual extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        );
+    }
 }
 
 let index = 0;
 
 var AwesomeProject = React.createClass({
-  getInitialState(){
-        return { rows: [] }
-    },
-
-  _addRow(){
-    this.state.rows.push(index++)
-    this.setState({ rows: this.state.rows })
-  },
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableHighlight onPress={ this._addRow } style={styles.button}>
-            <Text>Add new row</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <LoginScreen/>
+        );
+    }
 })
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  littlebox: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FF0000',
-  },
-  additembar: {
-    alignItems: 'flex-end',
-    backgroundColor: '#FF0000',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: '#F0F0F2'
+    },
+    titleBar: {
+        textAlign: 'center',
+        fontSize: 24,
+        paddingBottom: 4,
+        paddingTop: 4,
+        color: '#161616',
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#BCBCBC',
+        backgroundColor: '#F8F8F8',
+        marginBottom: 3,
+        alignSelf: 'stretch'
+    },
+    fieldLabel: {
+        fontSize: 18
+    },
+    loginContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'stretch'
+    },
+    textBox: {
+        alignSelf: 'stretch'
+    }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
